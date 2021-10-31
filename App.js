27cfb7b6c78dca48da84497bdda6d245d76434a6
Jalome <chjalome@gmail.com>
2021-11-01 00:22:42 +0200

    initial upload

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View,TouchableOpacity, Touchable,Text} from 'react-native';
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
+  Viro3DObject,
+  ViroAmbientLight
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
@@ -13,7 +15,7 @@ const HelloWorldSceneAR = () => {
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
     if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
+      setText('Testing AR');
     } else if (state === ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
@@ -27,19 +29,60 @@ const HelloWorldSceneAR = () => {
         position={[0, 0, -1]}
         style={styles.helloWorldTextStyle}
       />
+      {/* <View style={styles.controls}>
+
+      </View> */}
     </ViroARScene>
   );
 };
 
+const CarSceneAR = ()=>{
+
+  function onInitialized(state, reason) {
+    console.log(state);
+    console.log(reason);
+  }
+
+  return(
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      {/* Objects need lights to be visible! */}
+      <ViroAmbientLight color="#ffffff" />
+      <Viro3DObject
+        source={require('./assets/model2.obj')}
+        // resources={[require('./assets/materials.mtl')]}
+        position={[0, -2, -5]}
+        scale={[5, 5, 5]}
+        type="OBJ"
+        rotation={[0, 0, 0]}
+        dragType="FixedDistance" onDrag={()=>{}} 
+
+      />
+    </ViroARScene>
+  )
+}
+
 export default () => {
+  const [Scene,setScene] = useState('CarSceneAR');
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
+    <View style={styles.mainView}>
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: Scene === 'CarSceneAR'? CarSceneAR: HelloWorldSceneAR,
+        }}
+        style={styles.f1}
+      />
+      <View style={styles.controls}>
+          <TouchableOpacity onPress={()=>setScene('CarSceneAR')}>
+            <Text>Image</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=>setScene('HelloWorldSceneAR')}>
+            <Text>Text</Text>
+          </TouchableOpacity>
+
+      </View>
+    </View>
   );
 };
 
@@ -52,4 +95,15 @@ var styles = StyleSheet.create({
     textAlignVertical: 'center',
     textAlign: 'center',
   },
+  mainView:{
+    flex:1,
+  },
+  controls:{
+    width:'100%',
+    height:100,
+    backgroundColor:'#fff',
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between'
+  }
 });
