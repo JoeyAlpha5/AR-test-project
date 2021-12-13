f3e9ef1ac2ee2fadac4ed0d12977fade99dd90b4
Jalome <chjalome@gmail.com>
2021-12-13 13:31:04 +0200

    updating project

--- App.js
+++ App.js
@@ -3,81 +3,170 @@ import {StyleSheet, View,TouchableOpacity, Touchable,Text} from 'react-native';
 import {
   ViroARScene,
   ViroText,
-  ViroConstants,
   ViroARSceneNavigator,
   Viro3DObject,
-  ViroAmbientLight
+  ViroAmbientLight,
+  ViroMaterials,
+  ViroAnimations,
+  ViroBox
 } from '@viro-community/react-viro';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
 
+const CarSceneAR = (props)=>{
+  let data = props.sceneNavigator.viroAppProps;
+  const [text, setText] = useState('Initializing AR...');
+  const [position,setPosition] = useState([0, 0, -5]);
+  const [Scale,setScale] = useState([0.05, 0.05, 0.05]);
+  const [Rotation,setRotation] = useState([0, 0, 0]);
   function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Testing AR');
-    } else if (state === ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+    console.log(props.sceneNavigator.viroAppProps)
+  }
+
+
+  // on object drag
+  const onObjectDrag = (dragToPos)=>{
+    // console.log(dragToPos);
+    setPosition(dragToPos);
+  }
+
+  const onObjectPinch = (pinchState, scaleFactor, source) =>{
+    if(pinchState == 3) {
+      let scale_size = Scale[0] * scaleFactor;
+      // console.log(scale_size);
+      setScale([scale_size,scale_size,scale_size]);
     }
   }
 
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-      {/* <View style={styles.controls}>
+  
+  const onObjectRotation = (rotateState, rotationFactor, source)=>{
+    if (rotateState == 3) {
+      let rotation = Rotation[0]-rotationFactor;
+      console.log(rotationFactor);
+      setRotation([rotation,rotation,rotation]);
+    }
 
-      </View> */}
-    </ViroARScene>
-  );
-};
-
-const CarSceneAR = ()=>{
-
-  function onInitialized(state, reason) {
-    console.log(state);
-    console.log(reason);
   }
+
+
+  ViroMaterials.createMaterials({
+    /**
+     * We can also diffuse a texture here.
+     */
+    wood: {
+      diffuseTexture: require('./assets/tv/Old_Tv/TV_Body_material_Base_Color.png'),
+    }
+  });
+
+  ViroAnimations.registerAnimations({
+    /** To begin with we have added simple rotation animation */
+    rotate: {
+      properties: {
+      rotateY: '+=90',
+      },
+      duration: 2500, //.25 seconds
+    },
+  });
+  
 
   return(
     <ViroARScene onTrackingUpdated={onInitialized}>
       {/* Objects need lights to be visible! */}
       <ViroAmbientLight color="#ffffff" />
-      <Viro3DObject
-        source={require('./assets/model2.obj')}
-        // resources={[require('./assets/materials.mtl')]}
-        position={[0, -2, -5]}
-        scale={[5, 5, 5]}
-        type="OBJ"
-        rotation={[0, 0, 0]}
-        dragType="FixedDistance" onDrag={()=>{}} 
 
-      />
+      {data.type !== "text"?
+              <Viro3DObject
+                source={require('./assets/tv/Old_Tv/Old_Tv.obj')}
+                position={position}
+                scale={Scale}
+                type="OBJ"
+                rotation={Rotation}
+                // dragType="FixedDistance" 
+                onDrag={onObjectDrag} 
+                onPinch={onObjectPinch}
+                onRotate={onObjectRotation}
+                materials={['wood']}
+                animation={{name: 'rotate', run: true, loop: true}}
+
+
+            />
+            :
+            <ViroText
+              text={text}
+              scale={[0.5, 0.5, 0.5]}
+              position={[0, 0, -1]}
+              style={styles.helloWorldTextStyle}
+          />
+      }
+
     </ViroARScene>
   )
 }
 
+
+// 3d box 
+const BoxScene = (props)=>{
+
+    ViroAnimations.registerAnimations({
+      /** To begin with we have added simple rotation animation */
+      rotate: {
+        properties: {
+        rotateY: '+=90',
+        },
+        duration: 2500, //.25 seconds
+      },
+    });
+
+
+    ViroMaterials.createMaterials({
+      /**
+       * Material in its simplest form is just diffused color
+       */
+      white: {
+        diffuseColor: 'rgba(255,255,255,1)',
+      },
+      /**
+       * We can also diffuse a texture here.
+       */
+      wood: {
+        diffuseTexture: require('./assets/textures/wood.jpeg'),
+      }
+    });
+
+    return(
+      <ViroARScene>
+        <ViroAmbientLight color="#ffffff" intensity={200}/>
+
+          <ViroBox
+            height={2}
+            length={2}
+            width={2}
+            scale={[0.2, 0.2, 0.2]}
+            position={[0, -1, -1]}
+            animation={{name: 'rotate', run: true, loop: true}}
+            materials={['wood']}
+          />
+      </ViroARScene>
+    )
+}
+
 export default () => {
-  const [Scene,setScene] = useState('CarSceneAR');
+  const [Scene,setScene] = useState('text');
   return (
     <View style={styles.mainView}>
       <ViroARSceneNavigator
         autofocus={true}
         initialScene={{
-          scene: Scene === 'CarSceneAR'? CarSceneAR: HelloWorldSceneAR,
+          scene:BoxScene,
         }}
+        viroAppProps={{"type":Scene}}
         style={styles.f1}
       />
       <View style={styles.controls}>
-          <TouchableOpacity onPress={()=>setScene('CarSceneAR')}>
+          <TouchableOpacity onPress={()=>setScene('object')}>
             <Text>Image</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={()=>setScene('HelloWorldSceneAR')}>
+          <TouchableOpacity onPress={()=>setScene('text')}>
             <Text>Text</Text>
           </TouchableOpacity>
 
